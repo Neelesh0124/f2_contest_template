1839f489b4c3959b1fcbe23d92fc129a5c55cc04
neelesh0124 <neelesh0124@gmail.com>
2024-03-16 23:17:59 +0530

    FF

---
+++ script.js
@@ -0,0 +1,35 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+   let ans = arr.map(
+    if(arr[profession]==="developer")
+    console.log(ans)
+   )
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+   let ans = arr.forEach(
+    if(arr[profession]==="developer")
+    console.log(ans)
+   )
+}
+
+function addData() {
+  //Write your code here, just console.log
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
